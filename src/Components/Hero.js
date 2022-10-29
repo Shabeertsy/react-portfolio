21b0cfbc2940ad69ca00694cf3b8cec1a9d350ae
shabeertsy <shabeerpptsy@gmail.com>
2022-10-29 12:58:50 +0530

    landing page completely changed ,media query re-writed

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -10,69 +10,78 @@ function Hero() {
 
 
   return (
+
     <div>
-      <div className='landing-page'>
+
+      <div className='background'>
       </div>
+      
+     {/* main-background body */}
+      <div className='main-body'>
 
-      <div className='shadow'>
 
+        <div className='shadow-left'>
+          {/* name text animation  */}
+          <div className="">
+            <h1 className='hero-name responsive-text'>  <MovingComponent
+              type="fadeInFromLeft"
+              duration="1800ms"
+              delay="0s"
+              direction="normal"
+              timing="ease"
+              iteration="2"
+              fillMode="none">
+              SHABEER P P
+            </MovingComponent></h1>
+          </div>
 
-        {/* name text animation  */}
+          <div className="des-left">
+            <p className='description'>Build Amazing Responsive Website using react html and css  <br />Lorem ipsum dolor sit amet consectetur. <br /> Lorem ipsum dolor sit amet consectetur. </p>            
+          </div>
 
-        <h1 className='hero-name responsive-text'>  <MovingComponent
-          type="fadeInFromLeft"
-          duration="1800ms"
-          delay="0s"
-          direction="normal"
-          timing="ease"
-          iteration="2"
-          fillMode="none">
-          SHABEER P P
-        </MovingComponent></h1>
+        </div>
 
 
 
         {/* additional text typing animation */}
-        
-        <div className='right-animation'>
-          <h3 className='moving-text'>  <MovingComponent type="typewriter" className='anime'
-            dataText={[
-              'React',
-              'Python',
-              'CSS',
-              'JAVA SCRIPT'
-            ]} /></h3>
+        <div className="shadow-right">
+          <div className='right-animation'>
+            <h3 className='moving-text'>  <MovingComponent type="typewriter" className='anime'
+              dataText={[
+                'React',
+                'Python',
+                'CSS',
+                'JAVA SCRIPT'
+              ]} /></h3>
+          </div>
+
+          <div className='des-container'>
+
+
+            {/* rotating plus icon using react module */}
+
+            <FontAwesome
+
+              className='responsive-button '
+              name="plus"
+
+              size="2x"
+              spin
+              style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+            />
+
+
+
+          </div>
         </div>
-
-
-        {/* landing page description */}
-
-        <div className='des-container'>
-          <p className='description'>Build Amazing Responsive Website using react html and css  <br />Lorem ipsum dolor sit amet consectetur. <br /> Lorem ipsum dolor sit amet consectetur. </p>
-
-
-
-          {/* rotating plus icon using react module */}
-
-          <FontAwesome
-
-            className='responsive-button '
-            name="plus"
-
-            size="2x"
-            spin
-            style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
-          />
-
-        </div>
-
-
-        {/* landing page hire me button */}
-
-        <button className=' hero-button'>hire me</button>
-
-
       </div>
+
+      {/* landing page hire me button */}
+      <div className="button-div">
+        <button className=' hero-button'>hire me</button>
+      </div>
+
+
     </div>
   )
 }
