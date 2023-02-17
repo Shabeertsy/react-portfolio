493b0b28d9dfcf3e5be034eb1c434441ef6ca111
shabeertsy <shabeerpptsy@gmail.com>
2023-02-17 18:14:32 +0530

    final

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Route, Routes } from 'react-router-dom'
 import Projects from './Container/Projects';
 import Contact from './Container/Contact';
 import Myskills from './Container/Myskills';
-import Resume from './Container/Resume';
 
 
 
@@ -17,15 +16,14 @@ function App() {
     <div className="app">
       <div className="shadow">
 
-      <Navbar />
-      <Routes>
-        <Route path='/contacts' element={<Contact />} />
-        <Route path='/projects' element={<Projects/>} />
-        <Route path='/myskills' element={<Myskills />} />
-        <Route path='/' element={<Hero />} />
-        <Route path='/resume' element={<Resume />} />
-        
-      </Routes>
+      
+        <Routes>
+        <Navbar />
+          <Route exact path='/' element={<Hero />} />
+          <Route path='/contacts' element={<Contact />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/myskills' element={<Myskills />} />
+        </Routes>
       </div>
 
 
